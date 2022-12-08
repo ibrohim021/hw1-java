2c9af7611e2d04989a1e92d5f8b2a0c39ecf1930
ibrohim021 <dim_aviasky@mail.ru>
2022-12-08 20:52:59 +0300

    ex8

---
+++ ex7.java
@@ -0,0 +1,19 @@
+// Задать пустой целочисленный массив длиной 100.
+// С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+
+
+
+public class ex7 {
+    public static void main(String[] args) {
+        System.out.println("\nTask 2: ");
+        task2(100);
+    }
+
+    public static void task2(int size) {
+        int[] fillArr = new int[size];
+        for (int i = 0; i < fillArr.length; i++) {
+            fillArr[i] = i + 1;
+            System.out.print(fillArr[i] + " ");
+        }
+    }
+}
